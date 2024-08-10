dbe0e2bb6a7c6222a62d1c235864fceb74067810
Sean Baker <oceanapplications@gmail.com>
2024-08-09 18:54:15 -0600

    First Import

---
+++ README.md
@@ -0,0 +1,7 @@
+# Broken
+
+Install Dependencies and run npm run build
+
+Visit the home page which is resources/Pages/Welcome.vue
+
+Will return error Unable to locate file in Vite manifest: resources/js/Pages/Welcome.vue.
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {
+    Content,
+    fetchOneEntry,
+    isPreviewing,
+    getBuilderSearchParams,
+} from '@builder.io/sdk-vue';
+import { onMounted, ref } from 'vue';
+
+
+const BUILDER_PUBLIC_API_KEY = 'sdfgsdfgsdfgsdfg';
+const content = ref(null);
+const canShowContent = ref(false);
+const model = 'page';
+
+
+    content.value = await fetchOneEntry({
+        model,
+        apiKey: BUILDER_PUBLIC_API_KEY,
+        options: getBuilderSearchParams(new URL(location.href).searchParams),
+        userAttributes: {
+            urlPath: window.location.pathname,
+        },
+    });
+    canShowContent.value = content.value ? true : isPreviewing();
+</script>
+
+
+<template>
+
+
+</template>
+
+<style scoped>
+
+</style>
